0011903a9218e1e30d277e398fa4b6e04c91c294
Ghassan Achi <ghassan.achi@gmail.com>
2021-09-23 14:19:51 +0300

    chp4-p12: optimize by using the running_sum HashMap

--- src/problems/chp_4/problem_12.rs
+++ src/problems/chp_4/problem_12.rs
@@ -1,4 +1,6 @@
 use crate::structures::{BinaryTree, BinaryTreeUtil, TreeNodeRef};
+use std::collections::hash_map::Entry;
+use std::collections::HashMap;
 
 /// Since the question did not clarify I am making the assumption that a single node path is
 /// considered a valid path. If that were not the case, appending the node value to the path could
@@ -6,39 +8,44 @@ use crate::structures::{BinaryTree, BinaryTreeUtil, TreeNodeRef};
 /// remain the same.
 fn binary_path_sum_helper(
     node: TreeNodeRef<i32>,
-    paths: &mut Vec<i32>,
+    path_count: &mut HashMap<i32, u32>,
+    running_sum: i32,
     target_sum: i32,
-    valid_paths: &mut u32,
-) {
+) -> u32 {
     if let Some(node) = node {
-        let val = node.borrow().val;
-        paths.push(0);
-        for path in paths.iter_mut() {
-            *path += val;
-            if *path == target_sum {
-                *valid_paths += 1;
+        let node_val = node.borrow().val;
+        let running_sum = running_sum + node_val;
+
+        let sum = running_sum - target_sum;
+        let mut total_sum = *path_count.entry(sum).or_insert(0);
+
+        if running_sum == target_sum {
+            total_sum += 1;
+        }
+
+        *path_count.entry(running_sum).or_insert(0) += 1;
+        total_sum += binary_path_sum_helper(node.left(), path_count, running_sum, target_sum);
+        total_sum += binary_path_sum_helper(node.right(), path_count, running_sum, target_sum);
+        if let Entry::Occupied(mut o) = path_count.entry(running_sum) {
+            let val = o.get_mut();
+            *val -= 1;
+            if *val == 0 {
+                o.remove_entry();
             }
         }
-        // Check left tree
-        binary_path_sum_helper(node.left(), paths, target_sum, valid_paths);
-        binary_path_sum_helper(node.right(), paths, target_sum, valid_paths);
-        paths.pop();
-        for path in paths.iter_mut() {
-            *path -= val;
-        }
+        return total_sum;
     }
+    0
 }
 
 pub fn binary_path_sum(tree: &BinaryTree<i32>, target_sum: i32) -> u32 {
-    let valid_paths = &mut 0u32;
-    let paths = &mut Vec::<i32>::new();
+    let path_count = &mut HashMap::new();
     binary_path_sum_helper(
         tree.root.as_ref().map(|n| n.clone()),
-        paths,
+        path_count,
+        0,
         target_sum,
-        valid_paths,
-    );
-    *valid_paths
+    )
 }
 
 #[cfg(test)]
@@ -75,10 +82,28 @@ mod tests {
         assert_eq!(binary_path_sum(&tree, 3), 0);
     }
 
+    /// Example from the book (solution)
     #[test]
     fn binary_path_sum_3() {
-        let tree = BinaryTree::new();
-        assert_eq!(binary_path_sum(&tree, 0), 0);
-        assert_eq!(binary_path_sum(&tree, 1), 0);
+        let tree = BinaryTree::build(&[
+            Some(10),
+            Some(5),
+            Some(3),
+            Some(3),
+            None,
+            None,
+            Some(-2),
+            None,
+            None,
+            Some(2),
+            None,
+            Some(1),
+            None,
+            None,
+            Some(-3),
+            None,
+            Some(11),
+        ]);
+        assert_eq!(binary_path_sum(&tree, 8), 3);
     }
 }
