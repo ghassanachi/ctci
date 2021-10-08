e2ac825b1341c3b9430ce7ff5a709e2d01f976ed
Ghassan Achi <ghassan.achi@gmail.com>
2021-10-08 12:41:38 +0300

    chp8-p11: comments

--- src/problems/chp_8/problem_11.rs
+++ src/problems/chp_8/problem_11.rs
@@ -1,3 +1,9 @@
+/// This is a similar approach to a sieve.
+/// For each coin:
+/// 1. Loop from coin_value to amount
+///     a. Increment the dp array at index i by dp[i - coin], this is equavalent to taking
+///     0..=(amount/coin_value) coins given how it updates the array.
+/// 2. Return dp[amount]
 pub fn count_ways_change(amount: usize, coins: &[usize]) -> usize {
     let mut dp = vec![0; amount + 1];
     dp[0] = 1;
@@ -26,6 +32,7 @@ mod tests {
         assert_eq!(count_ways_change(4, &coins), 1);
         assert_eq!(count_ways_change(5, &coins), 2);
         assert_eq!(count_ways_change(6, &coins), 2);
+        assert_eq!(count_ways_change(7, &coins), 2);
         assert_eq!(count_ways_change(10, &coins), 4);
         assert_eq!(count_ways_change(25, &coins), 13);
     }
